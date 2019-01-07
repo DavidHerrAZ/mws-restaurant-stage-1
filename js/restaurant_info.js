6a67b598a366f1e03fc8501df2cd3971ad338935
David Herr <davidherraz@outlook.com>
2019-01-07 13:21:22 -0700

    Fix restaurant heading semantics per code review

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -4,7 +4,7 @@ var newMap;
 /**
  * Initialize map as soon as the page is loaded.
  */
-document.addEventListener("DOMContentLoaded", event => {
+document.addEventListener('DOMContentLoaded', event => {
   initMap();
 });
 
@@ -17,22 +17,22 @@ initMap = () => {
       // Got an error!
       console.error(error);
     } else {
-      self.newMap = L.map("map", {
+      self.newMap = L.map('map', {
         center: [restaurant.latlng.lat, restaurant.latlng.lng],
         zoom: 16,
         scrollWheelZoom: false
       });
       L.tileLayer(
-        "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}",
+        'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}',
         {
           mapboxToken:
-            "pk.eyJ1IjoiZGF2aWRoZXJyYXoiLCJhIjoiY2ptaWwwY2MzMDYzZzN1bzEyN2Nqd2I3MSJ9.cKNGvUy6dAZatnT-bQHmGw",
+            'pk.eyJ1IjoiZGF2aWRoZXJyYXoiLCJhIjoiY2ptaWwwY2MzMDYzZzN1bzEyN2Nqd2I3MSJ9.cKNGvUy6dAZatnT-bQHmGw',
           maxZoom: 18,
           attribution:
             'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
             '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
             'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          id: "mapbox.streets"
+          id: 'mapbox.streets'
         }
       ).addTo(newMap);
       fillBreadcrumb();
@@ -66,10 +66,10 @@ fetchRestaurantFromURL = callback => {
     callback(null, self.restaurant);
     return;
   }
-  const id = getParameterByName("id");
+  const id = getParameterByName('id');
   if (!id) {
     // no id found in URL
-    error = "No restaurant id in URL";
+    error = 'No restaurant id in URL';
     callback(error, null);
   } else {
     DBHelper.fetchRestaurantById(id, (error, restaurant) => {
@@ -88,19 +88,22 @@ fetchRestaurantFromURL = callback => {
  * Create restaurant HTML and add it to the webpage
  */
 fillRestaurantHTML = (restaurant = self.restaurant) => {
-  const name = document.getElementById("restaurant-name");
+  const name = document.getElementById('restaurant-name');
   name.innerHTML = restaurant.name;
 
-  const address = document.getElementById("restaurant-address");
+  const address = document.getElementById('restaurant-address');
   address.innerHTML = restaurant.address;
 
-  const image = document.getElementById("restaurant-img");
-  image.className = "restaurant-img";
+  const image = document.getElementById('restaurant-img');
+  image.className = 'restaurant-img';
   // Set restaurant image alt tag for accessibility
-  image.setAttribute("alt", restaurant.name + " restaurant's supplied banner image");
+  image.setAttribute(
+    'alt',
+    restaurant.name + " restaurant's supplied banner image"
+  );
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
-  const cuisine = document.getElementById("restaurant-cuisine");
+  const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
 
   // fill operating hours
@@ -114,16 +117,18 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 /**
  * Create restaurant operating hours HTML table and add it to the webpage.
  */
-fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => {
-  const hours = document.getElementById("restaurant-hours");
+fillRestaurantHoursHTML = (
+  operatingHours = self.restaurant.operating_hours
+) => {
+  const hours = document.getElementById('restaurant-hours');
   for (let key in operatingHours) {
-    const row = document.createElement("tr");
+    const row = document.createElement('tr');
 
-    const day = document.createElement("td");
+    const day = document.createElement('td');
     day.innerHTML = key;
     row.appendChild(day);
 
-    const time = document.createElement("td");
+    const time = document.createElement('td');
     time.innerHTML = operatingHours[key];
     row.appendChild(time);
 
@@ -135,18 +140,18 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  * Create all reviews HTML and add them to the webpage.
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
-  const container = document.getElementById("reviews-container");
-  const title = document.createElement("h2");
-  title.innerHTML = "Reviews";
+  const container = document.getElementById('reviews-container');
+  const title = document.createElement('h2');
+  title.innerHTML = 'Reviews';
   container.appendChild(title);
 
   if (!reviews) {
-    const noReviews = document.createElement("p");
-    noReviews.innerHTML = "No reviews yet!";
+    const noReviews = document.createElement('p');
+    noReviews.innerHTML = 'No reviews yet!';
     container.appendChild(noReviews);
     return;
   }
-  const ul = document.getElementById("reviews-list");
+  const ul = document.getElementById('reviews-list');
   reviews.forEach(review => {
     ul.appendChild(createReviewHTML(review));
   });
@@ -157,34 +162,34 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  * Create review HTML and add it to the webpage.
  */
 createReviewHTML = review => {
-  const li = document.createElement("li");
+  const li = document.createElement('li');
 
   /** Add a review card header to contain name & date with solid background*/
-  const reviewHead = document.createElement("div");
-  reviewHead.className = "review-head";
+  const reviewHead = document.createElement('div');
+  reviewHead.className = 'review-head';
   // Set tab-index to 0 to focus hits the review
-  reviewHead.setAttribute("tabindex", "0");
+  reviewHead.setAttribute('tabindex', '0');
   li.appendChild(reviewHead);
 
-  const name = document.createElement("p");
+  const name = document.createElement('p');
   name.innerHTML = review.name;
   /** Add class name for css styling & 50% width */
-  name.className = "review-name";
+  name.className = 'review-name';
   reviewHead.appendChild(name);
 
-  const date = document.createElement("p");
+  const date = document.createElement('p');
   date.innerHTML = review.date;
   /** Add class name for css styling & 50% width */
-  date.className = "review-date";
+  date.className = 'review-date';
   reviewHead.appendChild(date);
 
-  const rating = document.createElement("p");
+  const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
   /** Add class name for css styling to highlight orange */
-  rating.className = "review-rating";
+  rating.className = 'review-rating';
   li.appendChild(rating);
 
-  const comments = document.createElement("p");
+  const comments = document.createElement('p');
   comments.innerHTML = review.comments;
   li.appendChild(comments);
 
@@ -195,8 +200,8 @@ createReviewHTML = review => {
  * Add restaurant name to the breadcrumb navigation menu
  */
 fillBreadcrumb = (restaurant = self.restaurant) => {
-  const breadcrumb = document.getElementById("breadcrumb");
-  const li = document.createElement("li");
+  const breadcrumb = document.getElementById('breadcrumb');
+  const li = document.createElement('li');
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
 };
@@ -206,10 +211,10 @@ fillBreadcrumb = (restaurant = self.restaurant) => {
  */
 getParameterByName = (name, url) => {
   if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
+  name = name.replace(/[\[\]]/g, '\\$&');
   const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
     results = regex.exec(url);
   if (!results) return null;
-  if (!results[2]) return "";
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
 };
